eeaefc8c6081dbdc2cdbe2e10da69cae2269b557
Reed Xia <huaqin2005@gmail.com>
2018-05-04 15:52:08 +0800

    add test_baidu.py

---
+++ tests/test_baidu.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestBaiDu(unittest.TestCase):
+    # URL = Config().get('URL')
+    URL = "http://www.baidu.com"
+
+    locator_kw = (By.ID, 'kw')
+    locator_su = (By.ID, 'su')
+    locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
+
+    def setUp(self):
+        base_path = None
+        if os.name == 'nt':
+            base_path = os.path.dirname(os.path.abspath(__file__)) + '\\..\\'
+        else:
+            base_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+        sys.path.append(base_path)
+        # from utils.config_reader import ConfigReader
+        # config = ConfigReader(base_path)
+        #self.driver = webdriver.Chrome(executable_path=config.get_win_chrome_driver())
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.URL)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_search_0(self):
+        self.driver.find_element(*self.locator_kw).send_keys('selenium 测试')
+        self.driver.find_element(*self.locator_su).click()
+        time.sleep(2)
+        links = self.driver.find_elements(*self.locator_result)
+        for link in links:
+            print(link.text)
+
+    def test_search_1(self):
+        self.driver.find_element(*self.locator_kw).send_keys('Python selenium')
+        self.driver.find_element(*self.locator_su).click()
+        time.sleep(2)
+        links = self.driver.find_elements(*self.locator_result)
+        for link in links:
+            print(link.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils/config_reader.py
+++ utils/config_reader.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
 import configparser
+import os
 
 
-def get_mac_chrome_driver(base_path):
-    config = configparser.ConfigParser()
-    config.read(base_path + 'config/config.ini')
-    return config['driver']['driver_path']
+class ConfigReader(object):
+
+    def __init__(self, base_path):
+        self.__config_path = os.path.join(base_path, 'config', 'config.ini')
+        if os.path.exists(self.__config_path):
+            self.config = configparser.ConfigParser()
+            self.config.read(self.__config_path)
+        else:
+            raise FileNotFoundError(
+                'Config file is NOT present as "' + self.__config_path + '" !')
+
+    def get_mac_chrome_driver(self):
+        return self.config['mac']['chrome_driver_path']
+
+    def get_win_chrome_driver(self):
+        return self.config['win']['chrome_driver_path']
