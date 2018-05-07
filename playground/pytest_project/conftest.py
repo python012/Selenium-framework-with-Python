809e8339053bf327c8e56e055e9cac8443edd3bc
Reed Xia <huaqin2005@gmail.com>
2018-05-07 17:50:29 +0800

    add comment for scope of pytest.fixture

--- playground/pytest_project/conftest.py
+++ playground/pytest_project/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session") # scope could be function(default), class, module
 def get_name():
     from settings.values import NAME
     return NAME
