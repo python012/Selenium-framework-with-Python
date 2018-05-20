c8a16f5d89f072ca01a02389ac5e7e393981e3a3
Reed Xia <huaqin2005@gmail.com>
2018-05-20 20:51:01 +0800

    fix to run on firefox

--- tests/test_withfixture.py
+++ tests/test_withfixture.py
@@ -9,19 +9,26 @@ class TestBaiDu(object):
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
-    def test_search_0(self, chrome_driver): # chrome_driver from /conftest.py
-        chrome_driver.find_element(*self.locator_kw).send_keys(u'selenium 测试')
-        chrome_driver.find_element(*self.locator_su).click()
+    # def test_search_0(self, chrome_driver): # chrome_driver from /conftest.py
+    #     chrome_driver.find_element(*self.locator_kw).send_keys(u'selenium 测试')
+    #     chrome_driver.find_element(*self.locator_su).click()
+    #     time.sleep(2)
+    #     links = chrome_driver.find_elements(*self.locator_result)
+    #     for link in links:
+    #         logger.info(link.text)
+
+    # def test_search_1(self, chrome_driver):
+    #     chrome_driver.find_element(*self.locator_kw).send_keys('Python selenium')
+    #     chrome_driver.find_element(*self.locator_su).click()
+    #     time.sleep(2)
+    #     links = chrome_driver.find_elements(*self.locator_result)
+    #     for link in links:
+    #         logger.info(link.text)
+
+    def test_search_2(self, firefox_driver):
+        firefox_driver.find_element(*self.locator_kw).send_keys('Python selenium')
+        firefox_driver.find_element(*self.locator_su).click()
         time.sleep(2)
-        links = chrome_driver.find_elements(*self.locator_result)
+        links = firefox_driver.find_elements(*self.locator_result)
         for link in links:
             logger.info(link.text)
-
-    def test_search_1(self, chrome_driver):
-        chrome_driver.find_element(*self.locator_kw).send_keys('Python selenium')
-        chrome_driver.find_element(*self.locator_su).click()
-        time.sleep(2)
-        links = chrome_driver.find_elements(*self.locator_result)
-        for link in links:
-            logger.info(link.text)
-
