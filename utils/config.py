36cad2a4b56d480c7a85fb72bda957cd8bdb8e98
Reed Xia <huaqin2005@gmail.com>
2018-05-28 11:33:42 +0800

    hot fix importting logger object issue

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 import configparser
 import os
-from utils.log import logger
+# from utils.log import logger
 
 
 __base_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
@@ -17,18 +17,18 @@ __env_dist = os.environ
 
 if "BROWSER_VENDOR" in __env_dist:
     BROWSER_VENDOR = __env_dist['BROWSER_VENDOR']
-    logger.info("BROWSER_VENDOR is found in System environment variables")
-    logger.info("BROWSER_VENDOR is set to: " + BROWSER_VENDOR)
+    # logger.info("BROWSER_VENDOR is found in System environment variables")
+    # logger.info("BROWSER_VENDOR is set to: " + BROWSER_VENDOR)
 else:
     BROWSER_VENDOR = __config.get('DEFAULT', 'BROWSER_VENDOR')
-    logger.info("BROWSER_VENDOR is Not found in System environment variables, default value is used")
-    logger.info("BROWSER_VENDOR is set to: " + BROWSER_VENDOR)
+    # logger.info("BROWSER_VENDOR is Not found in System environment variables, default value is used")
+    # logger.info("BROWSER_VENDOR is set to: " + BROWSER_VENDOR)
 
 if "BROWSER_DRIVER" in __env_dist:
     BROWSER_DRIVER = __env_dist['BROWSER_DRIVER']
-    logger.info("BROWSER_DRIVER is found in System environment variables")
-    logger.info("BROWSER_DRIVER is set to: " + BROWSER_DRIVER)
+    # logger.info("BROWSER_DRIVER is found in System environment variables")
+    # logger.info("BROWSER_DRIVER is set to: " + BROWSER_DRIVER)
 else:
     BROWSER_DRIVER = __config.get('DEFAULT', 'BROWSER_DRIVER')
-    logger.info("BROWSER_DRIVER is Not found in System environment variables, default value is used")
-    logger.info("BROWSER_DRIVER is set to: " + BROWSER_DRIVER)
+    # logger.info("BROWSER_DRIVER is Not found in System environment variables, default value is used")
+    # logger.info("BROWSER_DRIVER is set to: " + BROWSER_DRIVER)
